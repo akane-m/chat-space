a8371f92f0e29c8e30a81a3b4ae735478077775b
akane-m <a2mt824nv@gmail.com>
2020-07-04 17:13:40 +0900

    add-send-message-api

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image) {
+      let html =
+      `<div class="Chat-main__messages__message">
+        <div class="Chat-main__messages__message__title">
+          <div class="Chat-main__messages__message__title__name">
+            ${message.user_name}
+          </div>
+          <div class="Chat-main__messages__message__title__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Chat-main__messages__message__content-box">
+          <p class="Chat-main__messages__message__content-box__content">
+            ${message.content}
+          </p>
+        <img class="Chat-main__messages__message__content-box__image" src="${message.image}">
+        </div>
+      </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="Chat-main__messages__message">
+        <div class="Chat-main__messages__message__title">
+          <div class="Chat-main__messages__message__title__name">
+            ${message.user_name}
+          </div>
+          <div class="Chat-main__messages__message__title__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Chat-main__messages__message__content-box">
+          <p class="Chat-main__messages__message__content-box__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.Chat-main__footer__form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.Chat-main__messages').append(html);
+      $('.Chat-main__messages').animate({ scrollTop: $('.Chat-main__messages')[0].scrollHeight });
+      $('.Chat-main__footer__form')[0].reset();
+      $('.Chat-main__footer__form__send-btn').prop('disabled', false);
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    })
+  })
+});
